8fa7e96d12559113687749061cb34b7bede85d10
trndav <farsiteguide@gmail.com>
2023-09-19 21:24:13 +0200

    Created courses table

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-//= require jquery
+import "jquery"
 import "@hotwired/turbo-rails"
 import "controllers"
-
-//= require materialize
+import "require materialize"
 
