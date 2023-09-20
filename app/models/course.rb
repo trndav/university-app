d272fd6899c0b8c67efdb76c8675a11d45b08a27
trndav <farsiteguide@gmail.com>
2023-09-20 11:03:22 +0200

    Add courses validation and courses elements in index.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
-    self.table_name = "courses"
+    validates :short_name, presence: true, length: { minimum: 3, maximum: 20 }
+    validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
