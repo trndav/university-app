b65b766e5bb0adffcc139e1cd31f8a10edda6d57
trndav <farsiteguide@gmail.com>
2023-09-21 21:12:34 +0200

    Created student log in routes and view

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,11 @@
+class LoginsController < ApplicationController
+    def new
+      # Your code here
+    end
+  
+    def create
+      # Your code here
+    end
+  
+    # Other actions as needed
+  end
