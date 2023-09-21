42e4c8fae6382b98390eaa195a43f1804b4854af
trndav <farsiteguide@gmail.com>
2023-09-21 16:16:15 +0200

    Added Bcrypt, password_digest field in students table

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,5 @@ class Student < ApplicationRecord
         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
         validates :email, presence: true, length: { maximum: 50 }, 
             uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+        has_secure_password
     end    
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_095022) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_135649) do
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_095022) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
