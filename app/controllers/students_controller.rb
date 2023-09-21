69f7c50c8795bece231b198336366c68e4910efd
trndav <farsiteguide@gmail.com>
2023-09-21 14:20:25 +0200

    DRY, partial for students_controller, students/edit and students/show

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,6 @@
 class StudentsController < ApplicationController
+    before_action :set_student, only: [:show, :edit, :update]
+
     def index
         @students = Student.all
     end
@@ -6,23 +8,24 @@ class StudentsController < ApplicationController
     def new
         @student = Student.new
     end
+
     def create
         @student = Student.new(student_params)
         if @student.save
             flash[:notice] = "You have successfully signed up."
-            redirect_to root_path
+            redirect_to @student
         else
             render "new"
         end
     end
+
     def show
-        @student = Student.find(params[:id])
     end
-    def edit 
-        @student = Student.find(params[:id])
+
+    def edit
     end
+
     def update
-        @student = Student.find(params[:id])
         if @student.update(student_params)
             flash[:notice] = "You have successfully updated your profile."
             redirect_to @student
@@ -30,8 +33,14 @@ class StudentsController < ApplicationController
             render "edit"
         end
     end
+
     private
+
     def student_params
         params.require(:student).permit(:name, :email)
+    end
+
+    def set_student
+        @student = Student.find(params[:id])
     end
 end
